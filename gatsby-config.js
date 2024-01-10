8d3c0a2d5b1b6d36873f664fbef59e9a6e36ff0a
wb <wubohy@hanmail.net>
2024-01-10 23:46:06 +0900

    배포 확인

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `원보현 Gatsby blog`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
